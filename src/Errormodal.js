a0879b42671d25ceab576593229fb645295b9da8
Darren <hldaz77@gmail.com>
2023-07-25 08:56:40 +0100

    modal now pops up

--- src/Errormodal.js
+++ src/Errormodal.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "./ErrorModal.css";
 
-export default function ErrorModal({ errorCode, errorMessage, onClose }) {
+export default function ErrorModal({ errorMessage, onClose }) {
   return (
-    <div className="error-modal">
-      <div className="error-modal-content">
-        <h2>That's not a city!</h2>
-        <p>Please try again.</p>
-        <p>{errorMessage}</p>
-        <button onClick={onClose}>Close</button>
+    <div className="error-modal-overlay">
+      <div className="error-modal">
+        <div className="error-modal-content">
+          <h2>That's not a city!</h2>
+          <p>Please try again with a different city name.</p>
+          <p>{errorMessage}</p>
+          <button onClick={onClose}>Close</button>
+        </div>
       </div>
     </div>
   );
